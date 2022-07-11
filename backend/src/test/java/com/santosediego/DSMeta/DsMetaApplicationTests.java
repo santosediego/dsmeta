ba4371963897abff65bbf2a4960513122e882426
Diego Santos <diegoeustaquidossantos@hotmail.com>
2022-07-11 08:50:44 -0300

    Project created

---
+++ backend/src/test/java/com/santosediego/DSMeta/DsMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosediego.DSMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
